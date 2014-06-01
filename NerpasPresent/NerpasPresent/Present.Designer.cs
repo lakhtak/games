a9e6b3ffc9bf8d64f9afa32b5c21255fadb32a90
lakhtak <o.usagi@yandex.ru>
2014-06-01 09:07:32 +0400

    gift

---
+++ NerpasPresent/NerpasPresent/Present.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace NerpasPresent
+{
+    partial class Present
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureKit = new System.Windows.Forms.PictureBox();
+            this.pictureTulen = new System.Windows.Forms.PictureBox();
+            this.pictureLosos = new System.Windows.Forms.PictureBox();
+            this.pictureUtya = new System.Windows.Forms.PictureBox();
+            this.pictureLakhtak = new System.Windows.Forms.PictureBox();
+            this.pictureHeart = new System.Windows.Forms.PictureBox();
+            this.buttonKit = new System.Windows.Forms.Button();
+            this.buttonLosos = new System.Windows.Forms.Button();
+            this.buttonUtya = new System.Windows.Forms.Button();
+            this.buttonLakhtak = new System.Windows.Forms.Button();
+            this.buttonTulen = new System.Windows.Forms.Button();
+            this.text = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureKit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureTulen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLosos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureUtya)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLakhtak)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureHeart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureKit
+            // 
+            this.pictureKit.BackColor = System.Drawing.Color.Transparent;
+            this.pictureKit.Image = global::NerpasPresent.Properties.Resources.kit;
+            this.pictureKit.Location = new System.Drawing.Point(-113, 1);
+            this.pictureKit.Name = "pictureKit";
+            this.pictureKit.Size = new System.Drawing.Size(350, 71);
+            this.pictureKit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureKit.TabIndex = 0;
+            this.pictureKit.TabStop = false;
+            // 
+            // pictureTulen
+            // 
+            this.pictureTulen.BackColor = System.Drawing.Color.Transparent;
+            this.pictureTulen.Image = global::NerpasPresent.Properties.Resources.tulen;
+            this.pictureTulen.Location = new System.Drawing.Point(-126, 93);
+            this.pictureTulen.Name = "pictureTulen";
+            this.pictureTulen.Size = new System.Drawing.Size(350, 234);
+            this.pictureTulen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureTulen.TabIndex = 1;
+            this.pictureTulen.TabStop = false;
+            // 
+            // pictureLosos
+            // 
+            this.pictureLosos.BackColor = System.Drawing.Color.Transparent;
+            this.pictureLosos.Image = global::NerpasPresent.Properties.Resources.losos;
+            this.pictureLosos.Location = new System.Drawing.Point(-51, 333);
+            this.pictureLosos.Name = "pictureLosos";
+            this.pictureLosos.Size = new System.Drawing.Size(250, 276);
+            this.pictureLosos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureLosos.TabIndex = 2;
+            this.pictureLosos.TabStop = false;
+            // 
+            // pictureUtya
+            // 
+            this.pictureUtya.BackColor = System.Drawing.Color.Transparent;
+            this.pictureUtya.Image = global::NerpasPresent.Properties.Resources.utya;
+            this.pictureUtya.Location = new System.Drawing.Point(675, 318);
+            this.pictureUtya.Name = "pictureUtya";
+            this.pictureUtya.Size = new System.Drawing.Size(200, 256);
+            this.pictureUtya.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureUtya.TabIndex = 3;
+            this.pictureUtya.TabStop = false;
+            this.pictureUtya.Tag = "";
+            // 
+            // pictureLakhtak
+            // 
+            this.pictureLakhtak.BackColor = System.Drawing.Color.Transparent;
+            this.pictureLakhtak.Image = global::NerpasPresent.Properties.Resources.lakhtak;
+            this.pictureLakhtak.Location = new System.Drawing.Point(616, 50);
+            this.pictureLakhtak.Name = "pictureLakhtak";
+            this.pictureLakhtak.Size = new System.Drawing.Size(450, 213);
+            this.pictureLakhtak.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureLakhtak.TabIndex = 4;
+            this.pictureLakhtak.TabStop = false;
+            // 
+            // pictureHeart
+            // 
+            this.pictureHeart.BackColor = System.Drawing.Color.Transparent;
+            this.pictureHeart.Image = global::NerpasPresent.Properties.Resources.heart;
+            this.pictureHeart.Location = new System.Drawing.Point(392, 129);
+            this.pictureHeart.Name = "pictureHeart";
+            this.pictureHeart.Size = new System.Drawing.Size(40, 40);
+            this.pictureHeart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureHeart.TabIndex = 5;
+            this.pictureHeart.TabStop = false;
+            this.pictureHeart.Visible = false;
+            // 
+            // buttonKit
+            // 
+            this.buttonKit.BackgroundImage = global::NerpasPresent.Properties.Resources.konvert;
+            this.buttonKit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonKit.Location = new System.Drawing.Point(135, 53);
+            this.buttonKit.Name = "buttonKit";
+            this.buttonKit.Size = new System.Drawing.Size(50, 34);
+            this.buttonKit.TabIndex = 11;
+            this.buttonKit.UseVisualStyleBackColor = true;
+            this.buttonKit.Click += new System.EventHandler(this.buttonKit_Click);
+            // 
+            // buttonLosos
+            // 
+            this.buttonLosos.BackgroundImage = global::NerpasPresent.Properties.Resources.konvert;
+            this.buttonLosos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLosos.Location = new System.Drawing.Point(159, 487);
+            this.buttonLosos.Name = "buttonLosos";
+            this.buttonLosos.Size = new System.Drawing.Size(50, 34);
+            this.buttonLosos.TabIndex = 12;
+            this.buttonLosos.UseVisualStyleBackColor = true;
+            this.buttonLosos.Click += new System.EventHandler(this.buttonLosos_Click);
+            // 
+            // buttonUtya
+            // 
+            this.buttonUtya.BackgroundImage = global::NerpasPresent.Properties.Resources.konvert;
+            this.buttonUtya.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUtya.Location = new System.Drawing.Point(645, 487);
+            this.buttonUtya.Name = "buttonUtya";
+            this.buttonUtya.Size = new System.Drawing.Size(50, 34);
+            this.buttonUtya.TabIndex = 13;
+            this.buttonUtya.UseVisualStyleBackColor = true;
+            this.buttonUtya.Click += new System.EventHandler(this.buttonUtya_Click);
+            // 
+            // buttonLakhtak
+            // 
+            this.buttonLakhtak.BackgroundImage = global::NerpasPresent.Properties.Resources.konvert;
+            this.buttonLakhtak.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLakhtak.Location = new System.Drawing.Point(675, 172);
+            this.buttonLakhtak.Name = "buttonLakhtak";
+            this.buttonLakhtak.Size = new System.Drawing.Size(50, 34);
+            this.buttonLakhtak.TabIndex = 14;
+            this.buttonLakhtak.UseVisualStyleBackColor = true;
+            this.buttonLakhtak.Click += new System.EventHandler(this.buttonLakhtak_Click);
+            // 
+            // buttonTulen
+            // 
+            this.buttonTulen.BackgroundImage = global::NerpasPresent.Properties.Resources.konvert;
+            this.buttonTulen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonTulen.Location = new System.Drawing.Point(56, 172);
+            this.buttonTulen.Name = "buttonTulen";
+            this.buttonTulen.Size = new System.Drawing.Size(50, 34);
+            this.buttonTulen.TabIndex = 15;
+            this.buttonTulen.UseVisualStyleBackColor = true;
+            this.buttonTulen.Click += new System.EventHandler(this.buttonTulen_Click);
+            // 
+            // text
+            // 
+            this.text.BackColor = System.Drawing.Color.Transparent;
+            this.text.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.text.Image = global::NerpasPresent.Properties.Resources.cloud;
+            this.text.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.text.Location = new System.Drawing.Point(215, 333);
+            this.text.Margin = new System.Windows.Forms.Padding(3, 0, 33, 30);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(454, 191);
+            this.text.TabIndex = 18;
+            this.text.Text = "!";
+            this.text.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.text.Visible = false;
+            this.text.Click += new System.EventHandler(this.text_Click);
+            // 
+            // Present
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::NerpasPresent.Properties.Resources.nerpa;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(854, 533);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.buttonTulen);
+            this.Controls.Add(this.buttonLakhtak);
+            this.Controls.Add(this.buttonUtya);
+            this.Controls.Add(this.buttonLosos);
+            this.Controls.Add(this.buttonKit);
+            this.Controls.Add(this.pictureHeart);
+            this.Controls.Add(this.pictureLakhtak);
+            this.Controls.Add(this.pictureUtya);
+            this.Controls.Add(this.pictureLosos);
+            this.Controls.Add(this.pictureTulen);
+            this.Controls.Add(this.pictureKit);
+            this.MaximizeBox = false;
+            this.Name = "Present";
+            this.Text = "НЕРПОЧКА! С ДНЁМ РОЖДЕНИЯ!";
+            this.Load += new System.EventHandler(this.Present_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureKit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureTulen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLosos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureUtya)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLakhtak)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureHeart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureKit;
+        private System.Windows.Forms.PictureBox pictureTulen;
+        private System.Windows.Forms.PictureBox pictureLosos;
+        private System.Windows.Forms.PictureBox pictureUtya;
+        private System.Windows.Forms.PictureBox pictureLakhtak;
+        private System.Windows.Forms.PictureBox pictureHeart;
+        private System.Windows.Forms.Button buttonKit;
+        private System.Windows.Forms.Button buttonLosos;
+        private System.Windows.Forms.Button buttonUtya;
+        private System.Windows.Forms.Button buttonLakhtak;
+        private System.Windows.Forms.Button buttonTulen;
+        private System.Windows.Forms.Label text;
+    }
+}
+
---
+++ NerpasPresent/NerpasPresent/Present.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Threading;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace NerpasPresent
+{
+    public partial class Present : Form
+    {
+        readonly Timer _timer = new Timer();
+        private const int TimeInterval = 400;
+        private const int WaitReading = 10 * 1000;
+
+        public Present()
+        {
+            InitializeComponent();
+            //var player = new SoundPlayer(Resources.klingon_style);
+            //player.PlayLooping();
+        }
+
+        private void buttonKit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonTulen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLosos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLakhtak_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUtya_Click(object sender, EventArgs e)
+        {
+            HideButtons();
+            ShowCloud(215, 366, 454, 146, "test", false);
+            Thread.Sleep(WaitReading);
+            text.Visible = false;
+            MoveAnimal(pictureUtya, 469, 172);
+            ShowHeart(1, 1);
+            MoveAnimal(pictureUtya, 675, 318);
+            ShowButtons();
+        }
+
+        private void ShowHeart(int x, int y)
+        {
+            Thread.Sleep(3000);
+        }
+
+        private void MoveAnimal(PictureBox pictureBox, int x, int y)
+        {
+            const int steps = 10;
+            const int moveSpeed = 200;
+            var oldX = pictureBox.Location.X;
+            var oldY = pictureBox.Location.Y;
+
+            var diffX = Math.Abs(oldX - x);
+            var stepX = oldX > x ? diffX / 10 : -diffX / 10;
+
+            var diffY = Math.Abs(oldY - y);
+            var stepY = oldY > y ? diffY / 10 : -diffY / 10;
+            
+            for (var i = 0; i < steps; i++)
+            {
+                var newX = pictureBox.Location.X + stepX;
+                if ((stepY > 0 && newX > x) || (stepY < 0 && newX < x)) newX = x;
+
+                var newY = pictureBox.Location.Y + stepY;
+                if ((stepY > 0 && newY > y) || (stepY < 0 && newY < y)) newY = y;
+
+                pictureBox.Location = new Point(pictureBox.Location.X + stepX, pictureBox.Location.Y + stepY);
+                pictureBox.Refresh();
+                Thread.Sleep(moveSpeed);
+            }
+        }
+
+        private void ShowCloud(int x, int y, int width, int height, string textToShow, bool flip)
+        {
+            text.Location = new Point(x, y);
+            text.Width = width;
+            text.Height = height;
+            text.Visible = true;
+            if (flip)
+            {
+                text.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                text.ImageAlign = ContentAlignment.MiddleLeft;
+            }
+            else
+            {
+                text.ImageAlign = ContentAlignment.MiddleRight;                
+            }
+            text.Refresh();
+        }
+
+        private void HideButtons()
+        {
+            buttonKit.Visible = false;
+            buttonLakhtak.Visible = false;
+            buttonUtya.Visible = false;
+            buttonLosos.Visible = false;
+            buttonTulen.Visible = false;
+            Refresh();
+        }
+
+        private void ShowButtons()
+        {
+            buttonKit.Visible = true;
+            buttonLakhtak.Visible = true;
+            buttonUtya.Visible = true;
+            buttonLosos.Visible = true;
+            buttonTulen.Visible = true;
+            Refresh();
+        }
+
+        private void Present_Load(object sender, EventArgs e)
+        {
+            _timer.Interval = TimeInterval;
+            _timer.Start();
+        }
+
+        private void text_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
